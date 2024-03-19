49886d9b98190df7faa0c956b094d89bc1555d94
valderosch <phonelix96@gmail.com>
2024-03-19 14:03:21 +0200

    runner

--- RubyForms/libs.rb
+++ RubyForms/libs.rb
@@ -1,2 +1,13 @@
-require ('stats.rb')
+require 'yaml'
+require 'json'
+require 'pathname'
 
+# Підключення класів
+require_relative 'quiz'
+require_relative 'config'
+require_relative 'question'
+require_relative 'question_data'
+require_relative 'input_reader'
+require_relative 'file_writter'
+require_relative 'stats'
+require_relative 'engine'
--- RubyForms/runner.rb
+++ RubyForms/runner.rb
@@ -1,1 +1,4 @@
+require_relative 'config'
+require_relative 'engine'
 
+Engine.new.run
